cabba80d558a4ca5c249aeaf8bf9336b32c697ee
RunKaRin <dlrjswnrla@naver.com>
2024-01-23 20:11:58 +0900

    2024.01.23 20:11 / 추상 클래스, 실체 클래스, 실행 클래스

---
+++ 01/chap07/src/sec03/exma01/PhoneExample.java
@@ -0,0 +1,11 @@
+package sec03.exma01;
+
+public class PhoneExample {
+    public static void main(String[] args) {
+        SmartPhone smartPhone = new SmartPhone("카린");
+
+        smartPhone.turnOn();
+        smartPhone.internetSearch();
+        smartPhone.turnOff();
+    }
+}
